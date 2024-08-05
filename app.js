b1d75740c8ce8f5fd81546d82dbc774e82278a32
gulluhuseyn <gulluhuseynova2@gmail.com>
2024-08-05 23:57:46 +0400

    navbar duzelis

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const lists = [...document.querySelectorAll("li")];
+const lists = [...document.querySelectorAll(".accordion_li")];
+
 lists.map((li) => {
   li.addEventListener("click", () => {
-    const p = li.querySelector("p");
-    const img = li.querySelector("img");
-    if (p.style.height && img.style.height && p.style.height !== "0px" && img.style.height !== "0px") {
-      p.style.height = "0px";
-      img.style.height = "0px"
-    } else {
-      [...document.querySelectorAll("li p")].map(
-        (p) => (p.style.height = "0px")
-      );
-      [...document.querySelectorAll("li img")].map(
-        (img) => (img.style.height = "0px")
-      );
-      p.style.height = p.scrollHeight + "px";
-      img.style.height = img.scrollHeight + "px";
+    const p = li.querySelector(".accordion_hide");
+    const icon = li.querySelector(".fa-solid");
+    if (p.style.height && p.style.height !== "0px") {
+      p.style.height = 0;
+      icon.classList.remove("fa-angle-up");
+      icon.classList.add("fa-angle-down");
+      return; 
     }
+    lists.forEach((otherLi) => {
+      if (otherLi !== li) {
+        const otherP = otherLi.querySelector(".accordion_hide");
+        const otherIcon = otherLi.querySelector(".fa-solid");
+        otherP.style.height = 0;
+        otherIcon.classList.remove("fa-angle-up");
+        otherIcon.classList.add("fa-angle-down");
+      }
+    });
+    p.style.height = p.scrollHeight + "px";
+    icon.classList.remove("fa-angle-down");
+    icon.classList.add("fa-angle-up");
   });
 });
