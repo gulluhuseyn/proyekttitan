d1cba739f9c518096bce08dc7792baf4add7e062
gulluhuseyn <gulluhuseynova2@gmail.com>
2024-08-19 13:03:52 +0400

    responsive3

--- app.js
+++ app.js
@@ -24,3 +24,10 @@ lists.map((li) => {
     icon.classList.add("fa-angle-up");
   });
 });
+
+const nav_links = document.querySelector(".nav_links ul");
+const menu_btn = document.getElementById("menu_btn");
+
+menu_btn.addEventListener("click", () =>{
+  nav_links.classList.toggle("active")
+})
